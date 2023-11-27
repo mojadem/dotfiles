1da3b4216690016bb4fec10fe09b7132af919c18
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-11-27 12:01:29 -0500

    changed from pure.fish prompt to starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,3 +10,10 @@ abbr --add gitst git status
 abbr --add gita. git add .
 abbr --add gitcm git commit -m
 abbr --add gitcam git commit -am
+
+set -gx PNPM_HOME "/Users/mojadem/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+
+starship init fish | source
