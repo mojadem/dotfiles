82938ebf9ff39c30cdedb6181d471fe3aa85f852
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-11-27 01:24:24 -0500

    added git abbreviations to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,11 @@ eval (/opt/homebrew/bin/brew shellenv)
 
 export (envsubst < $HOME/.dotfiles/variables.env)
 
-alias ls="eza --icons --oneline"
+alias ls="eza --icons --oneline --group-directories-first"
 alias la="ls -a"
+alias ll="ls -al"
+
+abbr --add gitst git status
+abbr --add gita. git add .
+abbr --add gitcm git commit -m
+abbr --add gitcam git commit -am
