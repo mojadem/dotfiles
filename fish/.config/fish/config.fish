859e305d15eb25bcd8ccda4ca2be52fa87d11f18
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-12-05 00:08:15 -0500

    modifications for device agnostic behavior

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,24 @@
 switch (uname)
     case Darwin
         eval (/opt/homebrew/bin/brew shellenv)
+    case Linux
+        alias bat="batcat"
 end
-
 
 export (envsubst < $HOME/.dotfiles/xdg.env)
 export (envsubst < $HOME/.dotfiles/variables.env)
+
+fish_add_path -g $PNPM_HOME $CARGO_HOME/bin
 
 alias ls="eza --icons --oneline --group-directories-first"
 alias la="ls -a"
 alias ll="ls -al --git"
 
-abbr --add gitst git status
-abbr --add gita. git add .
-abbr --add gitcm git commit -m
-abbr --add gitcam git commit -am
-abbr --add gitco git checkout
+abbr gitst git status
+abbr gita. git add .
+abbr gitcm git commit -m
+abbr gitcam git commit -am
+abbr gitco git checkout
 
 set fzf_fd_opts --hidden
 set fzf_preview_dir_cmd eza --icons --oneline --group-directories-first --color=always --all 
