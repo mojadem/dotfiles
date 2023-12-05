78ea6823c820b84b1f0b5d9ae279e95d6fa14022
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-12-04 22:07:34 -0500

    config.fish updates, set pnpm and cargo path in fish_variables, split variables.env into xdg.env

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,20 +1,24 @@
-eval (/opt/homebrew/bin/brew shellenv)
+switch (uname)
+    case Darwin
+        eval (/opt/homebrew/bin/brew shellenv)
+end
 
+
+export (envsubst < $HOME/.dotfiles/xdg.env)
 export (envsubst < $HOME/.dotfiles/variables.env)
 
 alias ls="eza --icons --oneline --group-directories-first"
 alias la="ls -a"
-alias ll="ls -al"
+alias ll="ls -al --git"
 
 abbr --add gitst git status
 abbr --add gita. git add .
 abbr --add gitcm git commit -m
 abbr --add gitcam git commit -am
+abbr --add gitco git checkout
 
-set -gx PNPM_HOME "/Users/mojadem/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
+set fzf_fd_opts --hidden
+set fzf_preview_dir_cmd eza --icons --oneline --group-directories-first --color=always --all 
 
 starship init fish | source
 enable_transience
