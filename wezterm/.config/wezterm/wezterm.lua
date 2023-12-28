0e1e39abb9c7a028c00480432c647ef04a665505
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-12-28 03:48:01 -0500

    changed to lazyvim and fira code

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,21 +1,21 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'Hack'
+config.font = wezterm.font 'FiraCode Nerd Font'
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 
-config.default_cursor_style = "SteadyBar"
+config.default_cursor_style = 'SteadyBar'
 
 -- config.enable_tab_bar = false
 
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 
 config.window_padding = {
-	left = '3cell',
-	right = '3cell',
-	top = '1.5cell',
-	bottom = '1.5cell',
+  left = '3cell',
+  right = '3cell',
+  top = '1.5cell',
+  bottom = '1.5cell',
 }
 
 return config
