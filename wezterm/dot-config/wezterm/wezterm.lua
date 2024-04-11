bcb634a24a0c28bfb6f7c44bc209ca55ffd8f7ca
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-04-10 23:33:22 -0400

    customized wezterm config, need to add panes

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,165 +1,85 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-local config = {}
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
+-- defaults
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.default_cursor_style = 'SteadyBar'
-config.font = wezterm.font({ family = 'FiraCode Nerd Font', scale = 1.2 })
+config.font = wezterm.font { family = 'Fira Code', scale = 1.2 }
 config.window_close_confirmation = 'AlwaysPrompt'
 config.window_decorations = 'RESIZE'
 
-config.leader = { key = "Space", mods = "CMD|SHIFT", timeout_milliseconds = 1000 }
--- below is from https://github.com/theopn/dotfiles/blob/main/wezterm/wezterm.lua
+-- tab bar
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  return { { Attribute = { Intensity = 'Bold' } }, { Text = ' ' .. tab.tab_index .. ' ' } }
+end)
+
+wezterm.on('update-right-status', function(window, pane)
+  local status = window:active_workspace()
+
+  if window:active_key_table() then
+    status = window:active_key_table()
+  end
+
+  if window:leader_is_active() then
+    status = '<leader>'
+  end
+
+  window:set_right_status(wezterm.format { { Text = status .. ' ' } })
+end)
+
+-- keys
+config.leader = { key = 'Space', mods = 'CMD|SHIFT' }
+
 config.keys = {
-    -- Send C-a when pressing C-a twice
-    { key = "c",          mods = "LEADER", action = act.ActivateCopyMode },
-    { key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
-
-    -- Pane keybindings
-    { key = "s",          mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-    { key = "v",          mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-    { key = "h",          mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-    { key = "j",          mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-    { key = "k",          mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-    { key = "l",          mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-    { key = "q",          mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
-    { key = "z",          mods = "LEADER", action = act.TogglePaneZoomState },
-    { key = "o",          mods = "LEADER", action = act.RotatePanes "Clockwise" },
-    -- We can make separate keybindings for resizing panes
-    -- But Wezterm offers custom "mode" in the name of "KeyTable"
-    { key = "r",          mods = "LEADER", action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
-
-    -- Tab keybindings
-    { key = "t",          mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-    { key = "[",          mods = "LEADER", action = act.ActivateTabRelative(-1) },
-    { key = "]",          mods = "LEADER", action = act.ActivateTabRelative(1) },
-    { key = "n",          mods = "LEADER", action = act.ShowTabNavigator },
-    {
-        key = "e",
-        mods = "LEADER",
-        action = act.PromptInputLine {
-            description = wezterm.format {
-                { Attribute = { Intensity = "Bold" } },
-                { Foreground = { AnsiColor = "Fuchsia" } },
-                { Text = "Renaming Tab Title...:" },
-            },
-            action = wezterm.action_callback(function(window, pane, line)
-                if line then
-                    window:active_tab():set_title(line)
-                end
-            end)
-        }
-    },
-    -- Key table for moving tabs around
-    { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
-    -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-    { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
-    { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
-
-    -- Lastly, workspace
-    { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
-
+  { key = 't', mods = 'LEADER', action = act.ActivateKeyTable { name = 'tab' } },
+  { key = 'w', mods = 'LEADER', action = act.ActivateKeyTable { name = 'workspace' } },
 }
--- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
-for i = 1, 9 do
-    table.insert(config.keys, {
-        key = tostring(i),
-        mods = "LEADER",
-        action = act.ActivateTab(i - 1)
-    })
-end
 
 config.key_tables = {
-    resize_pane = {
-        { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
-        { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
-        { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
-        { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
-        { key = "Escape", action = "PopKeyTable" },
-        { key = "Enter",  action = "PopKeyTable" },
+  tab = {
+    { key = 'n', action = act.SpawnTab 'CurrentPaneDomain' },
+    { key = 's', action = act.ShowTabNavigator },
+    {
+      key = 'r',
+      action = act.PromptInputLine {
+        description = wezterm.format { { Text = 'Renaming tab title...' } },
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      },
     },
-    move_tab = {
-        { key = "h",      action = act.MoveTabRelative(-1) },
-        { key = "j",      action = act.MoveTabRelative(-1) },
-        { key = "k",      action = act.MoveTabRelative(1) },
-        { key = "l",      action = act.MoveTabRelative(1) },
-        { key = "Escape", action = "PopKeyTable" },
-        { key = "Enter",  action = "PopKeyTable" },
-    }
+  },
+  workspace = {
+    { key = 'n', action = act.SwitchToWorkspace { spawn = { cwd = '~' } } },
+    { key = 's', action = act.ShowLauncherArgs { flags = 'WORKSPACES' } },
+    { key = 'd', action = act.SwitchToWorkspace { name = 'default' } },
+    {
+      key = 'r',
+      action = act.PromptInputLine {
+        description = wezterm.format { { Text = 'Renaming workspace title...' } },
+        action = wezterm.action_callback(function(window, pane, line)
+          if line then
+            wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+            wezterm.emit 'update-right-status'
+          end
+        end),
+      },
+    },
+  },
 }
 
--- Tab bar
--- I don't like the look of "fancy" tab bar
-config.use_fancy_tab_bar = false
-config.status_update_interval = 1000
-config.tab_bar_at_bottom = false
-wezterm.on("update-status", function(window, pane)
-    -- Workspace name
-    local stat = window:active_workspace()
-    local stat_color = "#f7768e"
-    -- It's a little silly to have workspace name all the time
-    -- Utilize this to display LDR or current key table name
-    if window:active_key_table() then
-        stat = window:active_key_table()
-        stat_color = "#7dcfff"
-    end
-    if window:leader_is_active() then
-        stat = "LDR"
-        stat_color = "#bb9af7"
-    end
+-- mux
+config.unix_domains = {
+  { name = 'unix' },
+}
 
-    local basename = function(s)
-        -- Nothing a little regex can't fix
-        return string.gsub(s, "(.*[/\\])(.*)", "%2")
-    end
-
-    -- Current working directory
-    local cwd = pane:get_current_working_dir()
-    if cwd then
-        if type(cwd) == "userdata" then
-            -- Wezterm introduced the URL object in 20240127-113634-bbcac864
-            cwd = basename(cwd.file_path)
-        else
-            -- 20230712-072601-f4abf8fd or earlier version
-            cwd = basename(cwd)
-        end
-    else
-        cwd = ""
-    end
-
-    -- Current command
-    local cmd = pane:get_foreground_process_name()
-    -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
-    cmd = cmd and basename(cmd) or ""
-
-    -- Time
-    local time = wezterm.strftime("%H:%M")
-
-    -- Left status (left of the tab line)
-    window:set_left_status(wezterm.format({
-        { Foreground = { Color = stat_color } },
-        { Text = "  " },
-        { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-        { Text = " |" },
-    }))
-
-    -- Right status
-    window:set_right_status(wezterm.format({
-        -- Wezterm has a built-in nerd fonts
-        -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
-        { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-        { Text = " | " },
-        { Foreground = { Color = "#e0af68" } },
-        { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-        "ResetAttributes",
-        { Text = " | " },
-        { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-        { Text = "  " },
-    }))
-end)
+config.default_gui_startup_args = { 'connect', 'unix' }
 
 return config
