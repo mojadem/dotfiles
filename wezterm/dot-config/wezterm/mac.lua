d73663f1690e6ab1dececbd9f4f0766e6c249c46
Matt Dembiczak <dembiczakmatt@gmail.com>
2024-05-29 11:27:49 -0400

    stopped using wezterm unix mux because of issue with nvim termsync
    see https://github.com/wez/wezterm/issues/4607

--- wezterm/dot-config/wezterm/mac.lua
+++ wezterm/dot-config/wezterm/mac.lua
@@ -2,12 +2,6 @@ local module = {}
 
 function module.apply_to_config(config)
   config.font_size = 14.0
-
-  config.unix_domains = {
-    { name = 'unix' },
-  }
-
-  config.default_gui_startup_args = { 'connect', 'unix' }
 end
 
 return module
