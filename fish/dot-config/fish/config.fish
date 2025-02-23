935f8a7fd41815760f3369c0ab7824e6e4d24c7b
Matt Dembiczak <matt@mojadem.dev>
2025-02-22 23:28:35 -0500

    change fish prompt to hydro

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -41,6 +41,14 @@ if type -q fisher
     fzf_configure_bindings --directory=\e\cF --history=\e\cH --variables=\e\cV --processes=\e\cP --git_log=\e\cL --git_status=\e\cS
     set fzf_preview_dir_cmd eza --icons --oneline --group-directories-first --color=always
     set fzf_fd_opts --follow
+    set -g hydro_color_pwd cyan
+    set -g hydro_color_git brblack
+    set -g hydro_color_prompt green
+    set -g hydro_color_duration yellow
+    set -g hydro_color_vi_mode_default white --bold
+    set -g hydro_color_vi_mode_insert brblue --bold
+    set -g hydro_color_vi_mode_replace brred --bold
+    set -g hydro_color_vi_mode_visual fe8019 --bold
 else
     echo 'fisher not installed, run `setup plugins`'
 end
