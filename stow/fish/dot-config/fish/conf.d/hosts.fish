aafb900149b5ffefae63482209a7932372d33dc2
Matt Dembiczak <matt@dembiczak.net>
2025-06-25 20:14:09 -0400

    fish: update hosts

--- stow/fish/dot-config/fish/conf.d/hosts.fish
+++ stow/fish/dot-config/fish/conf.d/hosts.fish
@@ -1,17 +1,28 @@
-# TODO: fix hostnames on all devices
-test $hostname = club || exit
+# trim ".local" suffix on macs
+set host (hostname | string replace '.local' '')
 
-function tv
-    if test $argv = on
-        swaymsg output HDMI-A-1 enable
-        return
-    end
+switch $host
+    case club
+        function tv
+            if test $argv = on
+                swaymsg output HDMI-A-1 enable
+                return
+            end
 
-    if test $argv = off
-        swaymsg output HDMI-A-1 disable
-        return
-    end
+            if test $argv = off
+                swaymsg output HDMI-A-1 disable
+                return
+            end
 
-    echo "invalid arg"
-    return 2
+            echo "invalid arg"
+            return 2
+        end
+    case diamond
+        fish_add_path ~/dev/khan/devtools/our-lovely-cli/bin
+        fish_add_path ~/dev/khan/devtools/ka-clone/bin
+        fish_add_path ~/dev/khan/devtools/khan-linter/bin
+        fish_add_path ~/dev/khan/devtools/google-cloud-sdk/bin
+        fish_add_path ~/dev/khan/devtools/khan-dotfiles/bin
+        fish_add_path ~/dev/khan/webapp/genfiles/go/bin
+        fish_add_path /opt/homebrew/opt/go@1.23/bin
 end
