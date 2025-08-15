afff3b7b21c0c2151b84fb48a13233b06fa949ad
Matt Dembiczak <matt@dembiczak.net>
2025-08-14 22:06:58 -0400

    fish: update wezterm integration

--- stow/fish/dot-config/fish/conf.d/wezterm.fish
+++ stow/fish/dot-config/fish/conf.d/wezterm.fish
@@ -4,13 +4,21 @@ end
 
 function __wezterm_set_user_var
     command -q base64 || return
-    printf "\033]1337;SetUserVar=%s=%s\007" "$argv[1]" (echo -n "$argv[2]" | base64)
+    printf "\033]1337;SetUserVar=%s=%s\007" \
+        "$argv[1]" \
+        (echo -n "$argv[2]" | base64 | string trim)
+end
+
+function __wezterm_user_vars_preexec --on-event fish_preexec
+    __wezterm_set_user_var command "$argv"
 end
 
 function __wezterm_user_vars_postexec --on-event fish_postexec
-    __wezterm_set_user_var cwd (pwd | string replace $HOME "~")
+    __wezterm_set_user_var command
+    __wezterm_set_user_var cwd (pwd)
 end
 
 # TODO: is it desired behavior that an event handler must be called manually
 # first to register the event handler?
+__wezterm_user_vars_preexec
 __wezterm_user_vars_postexec
