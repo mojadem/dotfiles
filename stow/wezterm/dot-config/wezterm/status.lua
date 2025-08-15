c129f56b08a007b5638a4cabee606c14d040b266
Matt Dembiczak <matt@dembiczak.net>
2025-08-14 22:05:42 -0400

    wezterm: update status line

--- stow/wezterm/dot-config/wezterm/status.lua
+++ stow/wezterm/dot-config/wezterm/status.lua
@@ -1,14 +1,29 @@
 local wezterm = require("wezterm")
 
+local function format_status(strings)
+	local valid_strings = {}
+	for _, s in ipairs(strings) do
+		if #s > 0 then
+			table.insert(valid_strings, s)
+		end
+	end
+
+	return table.concat(valid_strings, " ")
+end
+
 wezterm.on("update-status", function(window, pane)
 	local key_table = window:active_key_table() or ""
 	key_table = window:leader_is_active() and "leader" or key_table
+
+	local title = pane:get_title()
+
 	local cwd = pane:get_user_vars().cwd or ""
+	cwd = cwd:gsub(os.getenv("HOME") or "", "~")
+
 	local workspace = window:active_workspace()
 
 	window:set_right_status(wezterm.format({
-		{ Text = key_table .. " " },
-		{ Text = cwd .. " " },
+		{ Text = format_status({ key_table, title, cwd }) .. " " },
 		"ResetAttributes",
 		{ Attribute = { Intensity = "Bold" } },
 		{ Background = { Color = "#a89984" } },
