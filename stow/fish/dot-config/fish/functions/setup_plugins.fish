bb7402cb5c1f587dcfbfc471506ef34034819310
Matt Dembiczak <matt@dembiczak.net>
2025-08-20 15:29:46 -0400

    fish: no more plugins

--- stow/fish/dot-config/fish/functions/setup_plugins.fish
+++
@@ -1,5 +0,0 @@
-function setup_plugins
-    curl -fsSL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-    fisher update
-    echo setup_plugins complete
-end
