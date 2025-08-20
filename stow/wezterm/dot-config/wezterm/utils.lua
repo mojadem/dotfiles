336ea5948519d644ced9af5fc90fa19f05e71094
Matt Dembiczak <matt@dembiczak.net>
2025-08-19 21:58:49 -0400

    wezterm: fix table merging

--- stow/wezterm/dot-config/wezterm/utils.lua
+++ stow/wezterm/dot-config/wezterm/utils.lua
@@ -7,16 +7,26 @@ function M.basename(s)
 	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
+function M.append_arrays(destination, source)
+	for _, item in ipairs(source) do
+		table.insert(destination, item)
+	end
+end
+
 function M.merge_tables(destination, source)
 	for key, value in pairs(source) do
 		if type(value) == "table" and type(destination[key]) == "table" then
-			M.merge_tables(destination[key], value)
+			local is_array = #value > 0
+
+			if is_array then
+				M.append_arrays(destination[key], value)
+			else
+				M.merge_tables(destination[key], value)
+			end
 		else
 			destination[key] = value
 		end
 	end
-
-	return destination
 end
 
 return M
