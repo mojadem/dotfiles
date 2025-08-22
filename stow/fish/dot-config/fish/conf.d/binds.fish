0a678e563420ca5e39d8286365ddc000e9053e5e
Matt Dembiczak <matt@dembiczak.net>
2025-08-22 16:03:19 -0400

    fish: don't use vi keybinds in favor of alt-e

--- stow/fish/dot-config/fish/conf.d/binds.fish
+++ stow/fish/dot-config/fish/conf.d/binds.fish
@@ -1,6 +1,4 @@
-fish_vi_key_bindings
-
-bind --mode insert ctrl-f 'commandline --insert (fd | fzf)'
-bind --mode insert ctrl-alt-f 'commandline --insert (fd -IH | fzf)'
-bind --mode insert ctrl-h 'commandline --insert (history | fzf)'
-bind --mode insert ctrl-t tmux_sessionize
+bind ctrl-f 'commandline --insert (fd | fzf)'
+bind ctrl-alt-f 'commandline --insert (fd -IH | fzf)'
+bind ctrl-h 'commandline --insert (history | fzf)'
+bind ctrl-t tmux_sessionize
