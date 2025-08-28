007ef5b2c0cc3bf8b1311cf3b9b879508c816951
Matt Dembiczak <matt@dembiczak.net>
2025-08-28 11:50:51 -0400

    fish: don't exec into tmux to allow detaching

--- stow/fish/dot-config/fish/config.fish
+++ stow/fish/dot-config/fish/config.fish
@@ -4,8 +4,8 @@ set -g fish_key_bindings fish_default_key_bindings
 # enter tmux automatically
 if command -q tmux && status is-interactive && test -z $TMUX
     if tmux has-session -t default
-        exec tmux attach-session -t default
+        tmux attach-session -t default
     else
-        exec tmux new-session -s default
+        tmux new-session -s default
     end
 end
