474170721f00c948edaebd6e443edc4df2f6ca8f
Matt Dembiczak <matt@dembiczak.net>
2025-08-27 23:45:24 -0400

    fish: update colors function

--- stow/fish/dot-config/fish/functions/colors.fish
+++ stow/fish/dot-config/fish/functions/colors.fish
@@ -18,10 +18,16 @@ function colors
         brwhite
 
     for color in $colors
-        set_color $color
-        echo $color
-        set_color --bold $color
-        echo $color bold
-        set_color normal
+        set_color --background $color
+        for i in (seq 1 10)
+            printf ' '
+        end
+        set_color normal; printf ' '
+        set_color $color; printf '%-9s ' $color; set_color normal
+        set_color --bold $color; printf '%-14s ' "$color bold"; set_color normal
+        set_color --dim $color; printf '%-13s ' "$color dim"; set_color normal
+        set_color --italics $color; printf '%-17s ' "$color italics"; set_color normal
+        set_color --underline $color; printf '%s' "$color underline"; set_color normal
+        printf '\n'
     end
 end
