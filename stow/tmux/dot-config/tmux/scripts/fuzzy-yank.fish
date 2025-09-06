f03169d21e163edea85533e62685441f8ac75571
Matt Dembiczak <matt@dembiczak.net>
2025-09-06 12:00:09 -0400

    tmux: add fuzzy-yank script

---
+++ stow/tmux/dot-config/tmux/scripts/fuzzy-yank.fish
@@ -0,0 +1,26 @@
+set copy_patterns \
+    # links
+    'https?:\/\/[^>\)\s]+' \
+    # unix paths
+    '(\/|~\/|\.\.?\/)?([\w\.-]+\/)+[\w\.-]+' \
+    # sha1 hashes
+    '[0-9a-f]{7,40}'
+
+set pane_text (tmux capture-pane -p)
+set fzf_input
+
+for pattern in $copy_patterns
+    for match in (echo $pane_text | rg --only-matching --regexp=$pattern)
+        # This handles duplicates, as well as the case where part of a url is
+        # detected as a unix path.
+        if not echo $fzf_input | rg --quiet $match
+            set -a fzf_input $match
+        end
+    end
+end
+
+set selection (string join \n $fzf_input | fzf --tmux center)
+test -z $selection; and exit
+
+# TODO: support linux clipboard
+printf "%s" $selection | pbcopy
