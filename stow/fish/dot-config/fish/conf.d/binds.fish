d59a9c1ac069d59d48c07677d1674e55664f1e2c
Matt Dembiczak <matt@dembiczak.net>
2025-09-08 17:57:02 -0400

    fish: use fzf for history search bind

--- stow/fish/dot-config/fish/conf.d/binds.fish
+++ stow/fish/dot-config/fish/conf.d/binds.fish
@@ -1,2 +1,3 @@
 bind ctrl-f 'commandline --insert (fd | fzf)'
 bind ctrl-alt-f 'commandline --insert (fd -IH | fzf)'
+bind ctrl-r 'commandline --replace (history | fzf --scheme=history --query=(commandline))'
