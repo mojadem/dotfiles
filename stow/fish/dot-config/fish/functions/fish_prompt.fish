1fd52178edaadf96b900e448736b387fe1e893ea
Matt Dembiczak <matt@dembiczak.net>
2025-09-13 15:14:43 -0400

    fish: add simple custom prompt

---
+++ stow/fish/dot-config/fish/functions/fish_prompt.fish
@@ -0,0 +1,10 @@
+function fish_prompt
+    set last_pipestatus $pipestatus
+
+    set cwd (set_color cyan) (prompt_pwd)
+    set vcs (set_color brblack) (fish_vcs_prompt | string trim)
+    set err (string match --invert -q 0 $last_pipestatus && echo -s (set_color red) "[$last_pipestatus]")
+    set sym (set_color brwhite) '> '
+
+    echo -s $cwd $vcs $err $sym
+end
