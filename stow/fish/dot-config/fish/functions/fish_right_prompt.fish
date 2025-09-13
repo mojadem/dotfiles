e14587e336d8cd168539d0c52f986eabb060d5d0
Matt Dembiczak <matt@dembiczak.net>
2025-09-13 15:15:02 -0400

    fish: add private indicator to right prompt

--- stow/fish/dot-config/fish/functions/fish_right_prompt.fish
+++ stow/fish/dot-config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,13 @@
 function fish_right_prompt
+    set prompt
+
     if not test -z $VIRTUAL_ENV_PROMPT
-        echo "$(set_color yellow)($VIRTUAL_ENV_PROMPT)"
+        set -a prompt "($VIRTUAL_ENV_PROMPT)"
     end
+
+    if not test -z $fish_private_mode
+        set -a prompt "(private)"
+    end
+
+    echo (set_color brblack) $prompt
 end
