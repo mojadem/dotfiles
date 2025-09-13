edca5ac7ae672ca05a8fdd7eeacfd3b94225d42c
Matt Dembiczak <matt@dembiczak.net>
2025-09-13 16:55:13 -0400

    fish: update colors

--- stow/fish/dot-config/fish/conf.d/colors.fish
+++ stow/fish/dot-config/fish/conf.d/colors.fish
@@ -1,2 +1,5 @@
-set fish_color_command blue
-set fish_color_autosuggestion white
+set fish_color_command brcyan
+set fish_color_param cyan
+set fish_color_quote green
+set fish_color_comment brblack
+set fish_color_operator brblue
