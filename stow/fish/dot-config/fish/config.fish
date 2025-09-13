1a0a65023596ccb37a5b1cbf3af86637874d8c17
Matt Dembiczak <matt@dembiczak.net>
2025-09-13 16:56:56 -0400

    fish: remove auto-enter tmux

--- stow/fish/dot-config/fish/config.fish
+++ stow/fish/dot-config/fish/config.fish
@@ -1,11 +1,2 @@
 set -g fish_greeting
 set -g fish_key_bindings fish_default_key_bindings
-
-# enter tmux automatically
-if command -q tmux && status is-interactive && test -z $TMUX
-    if tmux has-session -t default
-        tmux attach-session -t default
-    else
-        tmux new-session -s default
-    end
-end
