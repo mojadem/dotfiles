a468f70659e4820c3d19a19a3234551d42fc1958
Matt Dembiczak <matt@dembiczak.net>
2025-09-13 15:13:28 -0400

    fish: add bind for `accept-autosuggestion`

--- stow/fish/dot-config/fish/conf.d/binds.fish
+++ stow/fish/dot-config/fish/conf.d/binds.fish
@@ -1,3 +1,4 @@
 bind ctrl-f 'commandline --insert (fd | fzf)'
 bind ctrl-alt-f 'commandline --insert (fd -IH | fzf)'
 bind ctrl-r 'commandline --replace (history | fzf --scheme=history --query=(commandline))'
+bind ctrl-y accept-autosuggestion
