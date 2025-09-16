49bceec4cb404de50ad0600c9340cc3759c5bc6d
Matt Dembiczak <matt@dembiczak.net>
2025-09-16 14:03:27 -0400

    fish: sort abbrs

--- stow/fish/dot-config/fish/conf.d/abbrs.fish
+++ stow/fish/dot-config/fish/conf.d/abbrs.fish
@@ -1,4 +1,4 @@
-abbr root "cd (git rev-parse --show-toplevel)"
-abbr lg lazygit
-abbr tma "fish ~/.config/tmux/scripts/fuzzy-find-session.fish" # TMux Attach
 abbr e $EDITOR
+abbr lg lazygit
+abbr root "cd (git rev-parse --show-toplevel)"
+abbr tma "fish ~/.config/tmux/scripts/fuzzy-find-session.fish" # TMux Attach
