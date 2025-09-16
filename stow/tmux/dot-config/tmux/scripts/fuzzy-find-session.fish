6f230c706f91c7f46e3ba71a9bdcfa24fd4c014a
Matt Dembiczak <matt@dembiczak.net>
2025-09-16 13:55:56 -0400

    tmux: update `fuzzy-find-session` script to work outside of tmux

--- stow/tmux/dot-config/tmux/scripts/fuzzy-find-session.fish
+++ stow/tmux/dot-config/tmux/scripts/fuzzy-find-session.fish
@@ -4,8 +4,11 @@ set repos (fd --type=directory --hidden --no-ignore --max-depth=4 --format='{//}
 set fzf_input
 
 for s in $sessions
-    set active_session (tmux display-message -p '#{session_name}')
-    test $s = $active_session; and continue
+    # Only filter out active session if we are in TMUX
+    if set -q TMUX
+        set active_session (tmux display-message -p '#{session_name}')
+        test $s = $active_session; and continue
+    end
     set -a fzf_input "session:$s"
 end
 
@@ -19,12 +22,19 @@ set selection (string join \n $fzf_input | fzf --tmux center,border-native)
 test -z $selection; and exit
 
 set selection (string split ':' $selection)
+set session
+
 if test $selection[1] = session
-    tmux switch-client -t $selection[2]
+    set session $selection[2]
 else if test $selection[1] = repo
     set dir (string replace '~' $HOME $selection[2])
     set session (basename $selection[2])
     set session (string replace --all '.' '' $session) # session names cannot contain '.'
     tmux new-session -d -c $dir -s $session
+end
+
+if set -q TMUX
     tmux switch-client -t $session
+else
+    tmux attach -t $session
 end
