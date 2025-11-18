5980703037ee03900acca31b88a3d00468f54dff
Matt Dembiczak <matt@dembiczak.net>
2025-11-18 14:03:42 -0500

    fish: format `FZF_DEFAULT_OPTS`

--- stow/fish/dot-config/fish/conf.d/env.fish
+++ stow/fish/dot-config/fish/conf.d/env.fish
@@ -46,7 +46,10 @@ set -x EDITOR kak
 set -x GPG_TTY (tty)
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
-set -x FZF_DEFAULT_OPTS "--layout=reverse --style=minimal --color=bg:black,fg:white,hl:green,bg+:#$GRUVBOX_BG_S,fg+:bright-white,hl+:green,prompt:cyan,input-fg:bright-white,pointer:white,marker:white"
+set -x FZF_DEFAULT_OPTS "\
+	--layout=reverse \
+	--style=minimal \
+	--color=bg:black,fg:white,hl:green,bg+:#$GRUVBOX_BG_S,fg+:bright-white,hl+:green,prompt:cyan,input-fg:bright-white,pointer:white,marker:white"
 
 type -q vivid && set -x LS_COLORS (vivid generate gruvbox-dark)
 
