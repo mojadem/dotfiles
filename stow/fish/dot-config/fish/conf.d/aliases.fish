6ca3568473131b35275b57a6a8cdb65b0aacd265
Matt Dembiczak <matt@dembiczak.net>
2025-11-18 13:27:13 -0500

    fish: set copy/paste aliases

--- stow/fish/dot-config/fish/conf.d/aliases.fish
+++ stow/fish/dot-config/fish/conf.d/aliases.fish
@@ -2,3 +2,6 @@ set ls_binary (test (uname) = Darwin; and echo "gls"; or echo "ls")
 
 alias ls "$ls_binary --color=auto --group-directories-first"
 alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
+
+alias copy (test (uname) = Darwin; and echo 'pbcopy'; or echo 'wl-copy')
+alias paste (test (uname) = Darwin; and echo 'pbpaste'; or echo 'wl-paste')
