89d953353d77c6afa574c065ed890a4a9fd56b5c
Matt Dembiczak <matt@dembiczak.net>
2025-12-04 17:05:36 -0500

    fish: remove tma abbr

--- stow/fish/dot-config/fish/config.fish
+++ stow/fish/dot-config/fish/config.fish
@@ -1,7 +1,6 @@
 abbr e $EDITOR
 abbr lg lazygit
 abbr root "cd (git rev-parse --show-toplevel)"
-abbr tma "fish ~/.config/tmux/scripts/fuzzy-find-session.fish" # TMux Attach
 
 set -g fish_greeting
 set -g fish_key_bindings fish_default_key_bindings
