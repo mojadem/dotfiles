ac2d78309d7237c07b0308023c3db544556dabd9
Matt Dembiczak <matt@dembiczak.net>
2025-12-04 16:53:06 -0500

    tmux: refactor `fuzzy-find-session.fish` to toggle search modes

--- stow/tmux/dot-config/tmux/scripts/fuzzy-find-session.fish
+++ stow/tmux/dot-config/tmux/scripts/fuzzy-find-session.fish
@@ -1,40 +1,20 @@
-set sessions (tmux ls -F '#{session_name}' 2>/dev/null)
-set repos (fd --type=directory --hidden --no-ignore --max-depth=4 --format='{//}' --glob .git $HOME/dev)
+set -x SESSION_CMD 'tmux ls -F "#{session_name}" | rg -v $(tmux display-message -p "#{session_name}")'
+set -x REPO_CMD 'fd -HI -t=d -d=3 --format={//} -g .git $HOME/dev | sed "s|$HOME|~|"'
 
-set fzf_input
+set selection (begin
+	eval $SESSION_CMD | fzf \
+		--tmux center,border-native \
+		--prompt='sessions> ' \
+		--header='fuzzy-find-session: ctrl-s for sessions, ctrl-r for repos' \
+		--bind 'ctrl-s:reload(eval $SESSION_CMD)+change-prompt(sessions> )' \
+		--bind 'ctrl-r:reload(eval $REPO_CMD)+change-prompt(repos> )' \
+		| sed "s|~|$HOME|"
+end)
+test -z "$selection" && exit
 
-for s in $sessions
-    # Only filter out active session if we are in TMUX
-    if set -q TMUX
-        set active_session (tmux display-message -p '#{session_name}')
-        test $s = $active_session; and continue
-    end
-    set -a fzf_input "session:$s"
+set session (basename $selection)
+if not tmux ls -F '#{session_name}' | rg -Fxq $session
+    set session (tmux new-session -d -c $selection -s $session -P -F '#{session_name}')
 end
 
-for r in $repos
-    contains (basename $r) $sessions; and continue
-    set r (string replace $HOME '~' $r)
-    set -a fzf_input "repo:$r"
-end
-
-set selection (string join \n $fzf_input | fzf --tmux center,border-native)
-test -z $selection; and exit
-
-set selection (string split ':' $selection)
-set session
-
-if test $selection[1] = session
-    set session $selection[2]
-else if test $selection[1] = repo
-    set dir (string replace '~' $HOME $selection[2])
-    # Session names cannot contain '.'
-    set session (basename $selection[2] | string replace --all '.' '' $session)
-    tmux new-session -d -c $dir -s $session
-end
-
-if set -q TMUX
-    tmux switch-client -t $session
-else
-    tmux attach -t $session
-end
+tmux switch-client -t $session
