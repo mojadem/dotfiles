9a586437af27f2b9b3574ea9e76cbff43c958d38
Matt Dembiczak <matt@dembiczak.net>
2025-12-07 22:05:53 -0500

    fish: add user and host to prompt

--- stow/fish/dot-config/fish/functions/fish_prompt.fish
+++ stow/fish/dot-config/fish/functions/fish_prompt.fish
@@ -1,10 +1,12 @@
 function fish_prompt
     set last_pipestatus $pipestatus
 
+    set user (set_color white) (whoami)
+    set host (prompt_hostname)
     set cwd (set_color cyan) (prompt_pwd)
     set vcs (set_color brblack) (fish_vcs_prompt | string trim)
     set err (string match --invert -q 0 $last_pipestatus && echo -s (set_color red) "[$last_pipestatus]")
     set sym (set_color brwhite) '> '
 
-    echo -ns $cwd $vcs $err $sym
+    echo -ns $user @ $host ' ' $cwd $vcs $err $sym
 end
