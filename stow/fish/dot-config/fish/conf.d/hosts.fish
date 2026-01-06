80a0c567ffa7d4543578ed12214e33b56d86e8c0
Matt Dembiczak <matt@dembiczak.net>
2026-01-06 11:04:42 -0500

    fish: fix paths on diamond

--- stow/fish/dot-config/fish/conf.d/hosts.fish
+++ stow/fish/dot-config/fish/conf.d/hosts.fish
@@ -15,11 +15,11 @@ switch (prompt_hostname)
             return 2
         end
     case diamond
-        fish_add_path ~/dev/khan/devtools/our-lovely-cli/bin
-        fish_add_path ~/dev/khan/devtools/ka-clone/bin
-        fish_add_path ~/dev/khan/devtools/khan-linter/bin
-        fish_add_path ~/dev/khan/devtools/google-cloud-sdk/bin
-        fish_add_path ~/dev/khan/devtools/khan-dotfiles/bin
+        fish_add_path ~/dev/khan/our-lovely-cli/bin
+        fish_add_path ~/dev/khan/ka-clone/bin
+        fish_add_path ~/dev/khan/khan-linter/bin
+        fish_add_path ~/dev/khan/google-cloud-sdk/bin
+        fish_add_path ~/dev/khan/khan-dotfiles/bin
         fish_add_path ~/dev/khan/webapp/genfiles/go/bin
         fish_add_path /opt/homebrew/opt/go@1.24/bin
 
