f3d4ea3fcbd01e6330d930b3a7968514fc284b19
Matt Dembiczak <matt@dembiczak.net>
2026-01-09 20:33:26 -0500

    fish: show nix in right prompt when in nix shell

--- stow/fish/dot-config/fish/functions/fish_right_prompt.fish
+++ stow/fish/dot-config/fish/functions/fish_right_prompt.fish
@@ -1,13 +1,17 @@
 function fish_right_prompt
     set prompt
 
-    if not test -z $VIRTUAL_ENV_PROMPT
-        set -a prompt "($VIRTUAL_ENV_PROMPT)"
+    if set -q VIRTUAL_ENV_PROMPT
+        set -a prompt '[venv]'
     end
 
-    if not test -z $fish_private_mode
-        set -a prompt "(private)"
+    if set -q IN_NIX_SHELL
+        set -a prompt '[nix]'
     end
 
-    echo (set_color brblack) $prompt
+    if set -q fish_private_mode
+        set -a prompt '[private]'
+    end
+
+    echo (set_color blue) $prompt
 end
