d89aeb165ba6208ac89fd853caa24fcd6f054941
Matt Dembiczak <matt@dembiczak.net>
2026-01-20 11:10:13 -0500

    fish: fix fzf multi select bind

--- stow/fish/dot-config/fish/config.fish
+++ stow/fish/dot-config/fish/config.fish
@@ -8,8 +8,8 @@ alias ls "$ls_binary --color=auto --group-directories-first"
 
 alias wget "wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
 
-bind ctrl-f 'commandline --insert (fd | fzf -m)'
-bind ctrl-alt-f 'commandline --insert (fd -IH | fzf -m)'
+bind ctrl-f 'commandline --insert (fd | fzf -m | string join " ")'
+bind ctrl-alt-f 'commandline --insert (fd -IH | fzf -m | string join " ")'
 bind ctrl-r 'commandline --replace (history | fzf --scheme=history --query=(commandline))'
 bind ctrl-y accept-autosuggestion
 
