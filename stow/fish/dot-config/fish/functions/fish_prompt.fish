40efeebfd48f71e28bea39f1ecb718ceb271e785
Matt Dembiczak <matt@dembiczak.net>
2026-01-23 12:36:00 -0500

    revert: fish: update prompt pwd to include 2 full directories
    This reverts commit ef2a6ce5972b488417d3a703499abdbcf44e6c25.

--- stow/fish/dot-config/fish/functions/fish_prompt.fish
+++ stow/fish/dot-config/fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt
 
     set user (set_color white) (whoami)
     set host (prompt_hostname)
-    set cwd (set_color cyan) (prompt_pwd -D 2)
+    set cwd (set_color cyan) (prompt_pwd)
     set vcs (set_color brblack) (fish_vcs_prompt | string trim)
     set err (string match --invert -q 0 $last_pipestatus && echo -s (set_color red) "[$last_pipestatus]")
     set sym (set_color brwhite) '> '
