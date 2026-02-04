154f2298fa8e24347802deb2f6edcd29e1d76cc1
Matt Dembiczak <matt@dembiczak.net>
2026-02-03 21:53:52 -0500

    fish: add bind for inserting from history without replacing entire commandline

--- stow/fish/dot-config/fish/conf.d/binds.fish
+++ stow/fish/dot-config/fish/conf.d/binds.fish
@@ -1,4 +1,5 @@
 bind ctrl-f 'commandline --insert (fd | fzf -m | string join " ")'
 bind ctrl-alt-f 'commandline --insert (fd -IH | fzf -m | string join " ")'
 bind ctrl-r 'commandline --replace (history | fzf --scheme=history --query=(commandline))'
+bind ctrl-alt-r 'commandline --insert (history | fzf --scheme=history)'
 bind ctrl-y accept-autosuggestion
