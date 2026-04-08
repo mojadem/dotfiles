a141fc17a3a8e44b3cce6b61a662559940de6894
Matt Dembiczak <matt@dembiczak.net>
2026-04-08 11:23:52 -0400

    fish: follow symlinks in alt fd bind

--- stow/fish/dot-config/fish/conf.d/binds.fish
+++ stow/fish/dot-config/fish/conf.d/binds.fish
@@ -1,5 +1,5 @@
 bind ctrl-f 'commandline --insert (fd | fzf -m | string join " ")'
-bind ctrl-alt-f 'commandline --insert (fd -IH | fzf -m | string join " ")'
+bind ctrl-alt-f 'commandline --insert (fd -HIL | fzf -m | string join " ")'
 bind ctrl-r 'commandline --replace (history | fzf --scheme=history --query=(commandline))'
 bind ctrl-alt-r 'commandline --insert (history | fzf --scheme=history)'
 bind ctrl-y accept-autosuggestion
