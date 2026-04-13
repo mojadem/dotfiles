e10e1b92449a493fa2943a5764d458ee787fbb87
Matt Dembiczak <matt@dembiczak.net>
2026-04-13 17:43:26 -0400

    nix: migrate spade to kiosk config

--- hosts/spade/default.nix
+++ hosts/spade/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ inputs, ... }:
 
 {
   networking.hostName = "spade";
@@ -8,34 +8,12 @@
     inputs.home-manager.nixosModules.home-manager
     ./hardware-configuration.nix
     ../../modules/nixos
-    ../../modules/nixos/moonlight.nix
     ../../modules/nixos/controller.nix
+    ../../modules/nixos/kiosk.nix
   ];
 
   home-manager.users.mojadem = ./home.nix;
 
   services.openssh.enable = true;
 
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.gamescope}/bin/gamescope -f -W 3840 -H 2160 -- ${pkgs.moonlight-qt}/bin/moonlight";
-        user = "mojadem";
-      };
-    };
-  };
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  # Enable low-latency audio. This may or may not be necessary.
-  security.rtkit.enable = true;
 }
---
+++ modules/nixos/kiosk.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+
+let
+  kodi = pkgs.kodi-gbm.withPackages (p: [ p.joystick ]);
+  kiosk-session = pkgs.writeShellScript "kiosk-session" ''
+    while true; do
+      ${pkgs.gamescope}/bin/gamescope -f -W 3840 -H 2160 -- ${pkgs.moonlight-qt}/bin/moonlight
+      ${kodi}/bin/kodi-standalone
+    done
+  '';
+in
+{
+  hardware.graphics = {
+    # Enables hardware acceleration.
+    enable = true;
+    extraPackages = with pkgs; [
+      # Provides Intel N95 hardware video decoding for moonlight.
+      intel-media-driver
+    ];
+  };
+
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = kiosk-session;
+      user = "mojadem";
+    };
+  };
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+  security.rtkit.enable = true;
+
+  users.users.mojadem.extraGroups = [
+    # Maybe needed for fixing RTSP handshake errors between sunshine/moonlight.
+    "audio"
+    # Needed for access input devices (/dev/input/*).
+    "input"
+    # Needed for hardware decoding in both gamescope and moonlight (/dev/dri/renderD*).
+    "render"
+  ];
+}
