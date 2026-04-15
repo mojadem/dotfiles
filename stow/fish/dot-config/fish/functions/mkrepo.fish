9acd85dc6edb3dcb0c2f1d6f7a6b41c4820c479d
Matt Dembiczak <matt@dembiczak.net>
2026-04-14 21:04:28 -0400

    fish: accept full slug in mkrepo

--- stow/fish/dot-config/fish/functions/mkrepo.fish
+++ stow/fish/dot-config/fish/functions/mkrepo.fish
@@ -1,6 +1,10 @@
 function mkrepo
-    set repo ~/dev/mojadem/$argv
+    if not string match -qr '^[^/]+/[^/]+$' $argv
+        echo "expected format 'owner/repo'"
+        return 1
+    end
 
+    set repo ~/dev/$argv
     mkdir $repo
     and git init $repo
 end
