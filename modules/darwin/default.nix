c6df17a4284acbb5efac8ffe8e6e2ec13e058910
Matt Dembiczak <matt@dembiczak.net>
2026-04-30 09:18:04 -0400

    nix: set up homebrew on darwin with ocr pkg

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -2,6 +2,16 @@
 
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
-
   users.users.${username}.home = "/Users/${username}/";
+
+  system.primaryUser = username; # needed for homebrew
+  homebrew = {
+    enable = true;
+    taps = [
+      "schappim/ocr"
+    ];
+    brews = [
+      "schappim/ocr/ocr"
+    ];
+  };
 }
