34b126c12fd8433dcc9a5e98766c0505dff9d656
Matt Dembiczak <matt@dembiczak.net>
2026-04-30 09:18:15 -0400

    hammerspoon: add bind for ocr

--- stow/hammerspoon/dot-hammerspoon/init.lua
+++ stow/hammerspoon/dot-hammerspoon/init.lua
@@ -10,3 +10,14 @@ transcribe_path = os.getenv("HOME") .. "/.config/scripts/transcribe.fish"
 hs.hotkey.bind({ "cmd", "ctrl" }, "w", function()
 	hs.task.new("/opt/homebrew/bin/fish", nil, { transcribe_path }):start()
 end)
+
+hs.hotkey.bind({ "cmd", "shift" }, "6", function()
+	hs.task
+		.new("/opt/homebrew/bin/ocr", function(exitCode, stdOut, stdErr)
+			if exitCode ~= 0 then
+				return
+			end
+			hs.task.new("pbcopy", nil, { stdOut }):start()
+		end, {})
+		:start()
+end)
