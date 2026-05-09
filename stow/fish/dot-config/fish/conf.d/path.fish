e9a0107951b4f7f0deb98f7679a4848db0c95f1d
Matt Dembiczak <matt@dembiczak.net>
2026-05-09 11:44:54 -0400

    fish: remove PNPM_HOME from path

--- stow/fish/dot-config/fish/conf.d/path.fish
+++ stow/fish/dot-config/fish/conf.d/path.fish
@@ -1,11 +1,3 @@
 fish_add_path ~/.local/bin
 fish_add_path $CARGO_HOME/bin
 fish_add_path $GOPATH/bin
-
-function _init_path --on-event init_done
-    # Ensure this is at the front of PATH so node versions installed with
-    # `pnpm env` are the default.
-    fish_add_path --move $PNPM_HOME
-
-    functions --erase _init_path
-end
