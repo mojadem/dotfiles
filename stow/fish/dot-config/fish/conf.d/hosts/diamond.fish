27fbf48c1106dda0edad96811caea0f5a9e54d2d
Matt Dembiczak <matt@dembiczak.net>
2026-06-11 15:20:10 -0400

    fish: add `deploy-status` function on diamond

--- stow/fish/dot-config/fish/conf.d/hosts/diamond.fish
+++ stow/fish/dot-config/fish/conf.d/hosts/diamond.fish
@@ -8,6 +8,22 @@ fish_add_path ~/dev/khan/webapp/genfiles/go/bin
 set -q JAVA_HOME || set -x JAVA_HOME (/usr/libexec/java_home)
 fish_add_path "/opt/homebrew/opt/openjdk@21/bin"
 
+function deploy-status
+    git symbolic-ref refs/remotes/origin/HEAD |
+        string match -r 'master|main' |
+        read main_name
+
+    set main "origin/$main_name"
+    set deploy deploy/mojadem
+    set comparison "$deploy...$main"
+
+    echo "$comparison"
+    git rev-list --count --left-right $comparison |
+        read ahead behind
+
+    echo "$ahead commits ahead, $behind commits behind"
+end
+
 function _init_mise --on-event init_done
     # TODO: switch to nixpkg instead of brew
     mise activate --shims fish | source
