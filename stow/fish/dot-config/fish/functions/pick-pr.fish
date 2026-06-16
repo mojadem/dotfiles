42eebc90c87d25a8bd89eb97da76f68f44654b35
Matt Dembiczak <matt@dembiczak.net>
2026-06-16 15:54:08 -0400

    fish: fix `pick-pr` function

--- stow/fish/dot-config/fish/functions/pick-pr.fish
+++ stow/fish/dot-config/fish/functions/pick-pr.fish
@@ -1,13 +1,38 @@
 function pick-pr
-    set author me
-    if test (count $argv) -ne 0
-        set author $argv
+    argparse --name=pick-pr h/help 's/state=' -- $argv
+    or return
+
+    if set --query _flag_help
+        echo 'usage: pick-pr [--state STATE] [AUTHOR]'
+        return 0
     end
 
-    gh search prs --author "@$author" --state open --json repository,number,title |
+    if test (count $argv) -gt 1
+        echo 'pick-pr: expected at most one AUTHOR' >&2
+        return 2
+    end
+    set author (test (count $argv) = 1; and echo $argv; or echo me)
+
+    set state (set -q _flag_state; and echo $_flag_state; or echo open)
+    if not contains -- $state open closed
+        echo 'pick-pr: --state must be open or closed' >&2
+        return 2
+    end
+
+    if test "$author" = me
+        begin
+            gh search prs --author @me --state $state --json repository,number,title
+            gh search prs --assignee @me --state $state --json repository,number,title
+        end |
+            jq -s 'add | unique_by(.repository.nameWithOwner, .number)'
+    else
+        gh search prs --author "@$author" --state $state --json repository,number,title
+    end |
         jq '.[] | "\(.repository.nameWithOwner)#\(.number)\t\(.title)"' -r |
-        fzf |
+        fzf -m |
         awk '{print $1}' |
-        string replace '#' ' ' |
-        xargs gh pr view -w -R
+        string replace --all '#' ' ' |
+        while read repo pr
+            gh pr view -w -R $repo $pr
+        end
 end
