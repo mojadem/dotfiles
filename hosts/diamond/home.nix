108e579a4fec66e0933b3cce770450a6d326a732
Matt Dembiczak <matt@dembiczak.net>
2026-06-24 10:34:02 -0400

    nix: add mark pkg to diamond

--- hosts/diamond/home.nix
+++ hosts/diamond/home.nix
@@ -1,9 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
     ../../modules/home
     ../../modules/home/darwin.nix
     ../../modules/home/email.nix
+  ];
+
+  home.packages = with pkgs; [
+    mark
   ];
 }
