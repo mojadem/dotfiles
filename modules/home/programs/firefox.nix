e8c47abcc38ffee7c63db690519418e7059f951e
Matt Dembiczak <matt@dembiczak.net>
2026-07-09 10:53:08 -0700

    nix(firefox): add tridactyl extension

--- modules/home/programs/firefox.nix
+++ modules/home/programs/firefox.nix
@@ -5,6 +5,7 @@ let
     "uBlock0@raymondhill.net" = "ublock-origin";
     "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
     "{fd4fdeb0-5a65-4978-81c5-3488d4d56426}" = "gruvboxtheme";
+    "tridactyl.vim@cmcaine.co.uk" = "tridactyl-vim";
   };
 in
 {
