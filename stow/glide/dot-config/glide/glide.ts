5ac7f9a8b56f7f5750591624d6655782f0f6cf9e
Matt Dembiczak <matt@dembiczak.net>
2026-07-27 12:20:16 -0400

    glide: add container extension

--- stow/glide/dot-config/glide/glide.ts
+++ stow/glide/dot-config/glide/glide.ts
@@ -6,6 +6,9 @@ glide.addons.install(
   "https://addons.mozilla.org/firefox/downloads/file/4875950/bitwarden_password_manager-2026.6.1.xpi",
   { private_browsing_allowed: true },
 );
+glide.addons.install(
+  "https://addons.mozilla.org/firefox/downloads/file/4867303/multi_account_containers-8.3.8.xpi",
+);
 
 glide.prefs.set("general.smoothScroll", false);
 glide.prefs.set("browser.toolbars.bookmarks.visibility", "never");
