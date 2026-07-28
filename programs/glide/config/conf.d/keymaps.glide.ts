8cb36431351cc430f27d127ad66024f3967f2793
Matt Dembiczak <matt@dembiczak.net>
2026-07-28 15:26:04 -0400

    glide: add detach tab keymap

--- programs/glide/config/conf.d/keymaps.glide.ts
+++ programs/glide/config/conf.d/keymaps.glide.ts
@@ -1,2 +1,11 @@
 glide.keymaps.set("normal", "<leader>c", "config_reload");
 glide.keymaps.set("normal", "<leader>t", "tab_new");
+
+glide.keymaps.set(
+  "normal",
+  "<leader>D",
+  ({ tab_id }) => {
+    browser.windows.create({ tabId: tab_id });
+  },
+  { description: "Detach the current tab into a new window" },
+);
