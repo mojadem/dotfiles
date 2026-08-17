f9546bf5eba10a57cd57362328dbc787c271bbc8
Matt Dembiczak <matt@dembiczak.net>
2026-08-17 12:56:15 -0400

    fish: set club pi wrapper

--- programs/fish/config/conf.d/hosts/club.fish
+++ programs/fish/config/conf.d/hosts/club.fish
@@ -1,5 +1,7 @@
 abbr syspend "sleep 5; systemctl suspend"
 
+alias pi "env OPENROUTER_API_KEY=(pass show api/openrouter) pi"
+
 if status is-login; and test (tty) = /dev/tty1
     function launch_environment --on-event init_done
         echo "Select environment:"
