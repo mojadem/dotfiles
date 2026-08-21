02fe5660dcbd03415226150bd9fe93a1724c36b3
Matt Dembiczak <matt@dembiczak.net>
2026-08-20 22:43:37 -0400

    fish: update environment launcher prompt

--- programs/fish/config/conf.d/hosts/club.fish
+++ programs/fish/config/conf.d/hosts/club.fish
@@ -4,7 +4,7 @@ alias pi "env OPENROUTER_API_KEY=(pass show api/openrouter) pi"
 
 if status is-login; and test (tty) = /dev/tty1
     function launch_environment --on-event init_done
-        read -P "exec sway? [y]/n > " choice
+        read -P "exec sway? [Y/n] " choice
         test -z $choice
         and exec sway
     end
