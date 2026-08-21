776aee8b5b48c0be35a31662f97f2692aeaf434a
Matt Dembiczak <matt@dembiczak.net>
2026-08-20 21:42:12 -0400

    fish: remove kde from environment launcher

--- programs/fish/config/conf.d/hosts/club.fish
+++ programs/fish/config/conf.d/hosts/club.fish
@@ -4,21 +4,8 @@ alias pi "env OPENROUTER_API_KEY=(pass show api/openrouter) pi"
 
 if status is-login; and test (tty) = /dev/tty1
     function launch_environment --on-event init_done
-        echo "Select environment:"
-        echo "1) Sway"
-        echo "2) KDE Plasma"
-        echo "*) cancel"
-
-        read -P "[1] > " choice
-        test -z $choice && set choice 1
-
-        switch $choice
-            case 1
-                exec sway
-            case 2
-                exec dbus-run-session startplasma-wayland
-            case *
-                # Continue to shell.
-        end
+        read -P "exec sway? [y]/n > " choice
+        test -z $choice
+        and exec sway
     end
 end
