79f497ac312cf4d19218a4d6aff8fe36a0cbff5c
Matt Dembiczak <matt@dembiczak.net>
2026-09-09 15:29:58 -0400

    flake: enable prettier in treefmt for ts and json files

--- treefmt.nix
+++ treefmt.nix
@@ -5,5 +5,12 @@
     fish_indent.enable = true;
     just.enable = true;
     nixfmt.enable = true;
+    prettier = {
+      enable = true;
+      includes = [
+        "*.ts"
+        "*.json"
+      ];
+    };
   };
 }
