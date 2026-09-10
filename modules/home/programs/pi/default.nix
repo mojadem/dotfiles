378d673b26eff4669f67cc5efa57281c5426dd53
Matt Dembiczak <matt@dembiczak.net>
2026-09-10 10:40:51 -0400

    pi: convert to out-of-store symlink

--- modules/home/programs/pi/default.nix
+++ modules/home/programs/pi/default.nix
@@ -1,8 +1,20 @@
-{ ... }:
+{ config, ... }:
 
+let
+  configDir = "${config.home.homeDirectory}/dev/mojadem/dotfiles/modules/home/programs/pi/config";
+
+  link = name: {
+    source = config.lib.file.mkOutOfStoreSymlink "${configDir}/${name}";
+  };
+in
 {
-  home.file.".pi/agent" = {
-    source = ./config;
-    recursive = true;
+  home.file = {
+    ".pi/agent/AGENTS.md" = link "AGENTS.md";
+    ".pi/agent/APPEND_SYSTEM.md" = link "APPEND_SYSTEM.md";
+    ".pi/agent/keybindings.json" = link "keybindings.json";
+
+    ".pi/agent/extensions" = link "extensions";
+    ".pi/agent/prompts" = link "prompts";
+    ".pi/agent/themes" = link "themes";
   };
 }
