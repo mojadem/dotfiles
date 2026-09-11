e1e502d2b413709328db3bf93296ddb88642aecd
Matt Dembiczak <matt@dembiczak.net>
2026-09-11 14:04:12 -0400

    flake: fmt

--- modules/home/programs/fish/config/functions/colors.fish
+++ modules/home/programs/fish/config/functions/colors.fish
@@ -22,12 +22,29 @@ function colors
         for i in (seq 1 10)
             printf ' '
         end
-        set_color normal; printf ' '
-        set_color $color; printf '%-9s ' $color; set_color normal
-        set_color --bold $color; printf '%-14s ' "$color bold"; set_color normal
-        set_color --dim $color; printf '%-13s ' "$color dim"; set_color normal
-        set_color --italics $color; printf '%-17s ' "$color italics"; set_color normal
-        set_color --underline $color; printf '%s' "$color underline"; set_color normal
+
+        set_color normal
+        printf ' '
+        set_color $color
+        printf '%-9s ' $color
+        set_color normal
+
+        set_color --bold $color
+        printf '%-14s ' "$color bold"
+        set_color normal
+
+        set_color --dim $color
+        printf '%-13s ' "$color dim"
+        set_color normal
+
+        set_color --italics $color
+        printf '%-17s ' "$color italics"
+        set_color normal
+
+        set_color --underline $color
+        printf '%s' "$color underline"
+        set_color normal
+
         printf '\n'
     end
 end
